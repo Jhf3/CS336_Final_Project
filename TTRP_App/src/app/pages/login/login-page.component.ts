788a2bdf25d93f73a700bc53980a7abdee268395
MrCatMan49 <159658897+MrCatMan49@users.noreply.github.com>
2025-12-09 11:43:10 -0500

    removed login components

--- TTRP_App/src/app/pages/login/login-page.component.ts
+++ TTRP_App/src/app/pages/login/login-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { Login } from '../../components/login/login';
+import { Login } from '../../components/login/login/login';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [Login],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  template: '<app-login></app-login>',
+  styles: [`
+    :host {
+      display: block;
+      width: 100%;
+      height: 100vh;
+    }
+  `]
 })
 export class LoginPageComponent {
-
 }
