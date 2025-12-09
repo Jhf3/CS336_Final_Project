a2f4466ffc0930c7795302ca7fe2e0b9ea2ac35c
Simon <spt6@calvin.edu>
2025-12-09 11:29:02 -0500

    Removed testing page

--- TTRP_App/src/app/app.routes.ts
+++ TTRP_App/src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CampaignHistoryComponent } from './pages/campaign-history/campaign-history.component';
 import { DebugComponent } from './pages/debug/debug.component';
-import { TestingComponent } from './pages/testing/testing.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'campaign-history', component: CampaignHistoryComponent },
   { path: 'debug', component: DebugComponent },
-  { path: 'testing', component: TestingComponent },
   { path: '**', redirectTo: '' }
 ];
